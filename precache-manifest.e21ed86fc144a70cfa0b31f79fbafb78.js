ab3178f927a1ffab80e2c2d1f6434d23505d4658
Nathan Chacon <nathanchacon321@gmail.com>
2020-01-16 00:25:54 -0300

    Updates

---
+++ precache-manifest.e21ed86fc144a70cfa0b31f79fbafb78.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2035f718c8739c7f3e81456346dd3f41",
+    "url": "/Travel/index.html"
+  },
+  {
+    "revision": "b64865c66579cef3e03e",
+    "url": "/Travel/static/css/main.46477cfa.chunk.css"
+  },
+  {
+    "revision": "1e56969c02aaeb9c3ee0",
+    "url": "/Travel/static/js/2.3b1ccc2d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Travel/static/js/2.3b1ccc2d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b64865c66579cef3e03e",
+    "url": "/Travel/static/js/main.30d9c63e.chunk.js"
+  },
+  {
+    "revision": "a16d544c0473656df944",
+    "url": "/Travel/static/js/runtime-main.399e8b6e.js"
+  },
+  {
+    "revision": "c279cfff2bf15129c4c913f6f7263902",
+    "url": "/Travel/static/media/camping2.c279cfff.jpg"
+  },
+  {
+    "revision": "0ab96f3e574bfe1e33a65ab155395485",
+    "url": "/Travel/static/media/canyon.0ab96f3e.jpg"
+  },
+  {
+    "revision": "02e5e8bc533fd7e6d926113ba984c5df",
+    "url": "/Travel/static/media/cristo.02e5e8bc.jpg"
+  },
+  {
+    "revision": "d3afb507063e563bfa7fcb3d509d545f",
+    "url": "/Travel/static/media/lisboa.d3afb507.jpg"
+  },
+  {
+    "revision": "74a73798c887988588bfdcfc05233333",
+    "url": "/Travel/static/media/safari.74a73798.jpg"
+  },
+  {
+    "revision": "597a34877b70095e038cf68ea24d85b4",
+    "url": "/Travel/static/media/tokyo.597a3487.jpg"
+  },
+  {
+    "revision": "814f7261897270d39d490df81dbec479",
+    "url": "/Travel/static/media/torreEiffel.814f7261.jpg"
+  }
+]);
